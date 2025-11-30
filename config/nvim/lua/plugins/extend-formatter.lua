cd5fd5dd5257a7d7f0e95e45f31ae96899ced4d6
Liam Lin <zhonglin0107@gmail.com>
2025-11-30 11:39:16 +0800

    chore(nvim): update

--- config/nvim/lua/plugins/extend-formatter.lua
+++ config/nvim/lua/plugins/extend-formatter.lua
@@ -8,6 +8,7 @@ return {
           ["cpp"] = { "clang_format" },
           ["c++"] = { "clang_format" },
           -- ["cmake"] = { "gersemi" },
+          ["zsh"] = { "shfmt" },
         },
       },
     },
