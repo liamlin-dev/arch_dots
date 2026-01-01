024e60be322d16d18f73b1b1151c4e4954002db6
Liam Lin <zhonglin0107@gmail.com>
2026-01-01 21:34:41 +0800

    chore(nvim): use default blink keymap

--- config/nvim/lua/plugins/extend-blink.lua
+++ config/nvim/lua/plugins/extend-blink.lua
@@ -62,26 +62,22 @@ return {
 
       keymap = {
         preset = "none",
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide", "fallback" },
+        ["<C-y>"] = { "select_and_accept", "fallback" },
 
-        ["<C-k>"] = { "select_prev", "fallback" },
-        ["<C-j>"] = { "select_next", "fallback" },
-        ["<C-p>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+
+        ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+        ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
         ["<C-b>"] = { "scroll_documentation_up", "fallback" },
         ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-        ["<C-n>"] = { "show_signature", "hide_signature", "fallback" },
-        ["<C-c>"] = { "hide", "fallback" },
 
-        ["<Tab>"] = {
-          function(cmp)
-            if cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
-            end
-          end,
-          "snippet_forward",
-          "fallback",
-        },
+        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+
+        ["<Tab>"] = { "snippet_forward", "fallback" },
         ["<S-Tab>"] = { "snippet_backward", "fallback" },
       },
     },
