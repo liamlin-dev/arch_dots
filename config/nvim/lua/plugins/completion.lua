ad8a4afffa974a230af9849dcb3d967bbda4253d
Liam Lin <zhonglin0107@gmail.com>
2026-01-17 22:29:25 +0800

    feat(nvim): add blink word cmp provider

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -8,75 +8,53 @@ return {
         config = true,
       },
       "kristijanhusak/vim-dadbod-completion",
-      "fang2hou/blink-copilot",
-      "Kaiser-Yang/blink-cmp-avante",
+      "archie-judd/blink-cmp-words",
     },
-    opts = {
-      completion = {
-        keyword = { range = "full" },
-        accept = { auto_brackets = { enabled = true } },
-        list = { selection = { preselect = true, auto_insert = true } },
-        menu = {
-          auto_show = true,
-          border = "single",
-          draw = {
-            columns = { { "kind_icon" }, { "label", "source_id", gap = 1 } },
-            components = {
-              label = {
-                text = function(ctx)
-                  return require("colorful-menu").blink_components_text(ctx)
-                end,
-                highlight = function(ctx)
-                  return require("colorful-menu").blink_components_highlight(ctx)
-                end,
-              },
-            },
-            treesitter = { "lsp" },
-          },
-        },
-        documentation = {
-          window = {
-            border = "single",
-          },
-          auto_show = true,
-          auto_show_delay_ms = 100,
-        },
-        ghost_text = { enabled = false }, -- conflict with menu auto show
-      },
+    opts = function(_, opts)
+      local util = require("util.blink")
 
-      cmdline = {
-        enabled = true,
+      return {
+        sources = {
+          -- `lsp`, `buffer`, `snippets`, `path` and `omni` are built-in
+          default = { "lsp", "buffer", "snippets", "path", "dictionary" },
+
+          per_filetype = {
+            sql = { "snippets", "dadbod", "buffer" },
+            lua = { inherit_defaults = true, "lazydev" },
+          },
+
+          providers = {
+            dadbod = { module = "vim_dadbod_completion.blink" },
+            lazydev = util.providers.lazydev,
+            dictionary = util.providers.dictionary,
+          },
+        },
+
+        completion = {
+          keyword = { range = "full" },
+          accept = { auto_brackets = { enabled = true } },
+          list = { selection = { preselect = true, auto_insert = true } },
+          ghost_text = { enabled = false }, -- conflict with menu auto show
+          menu = util.menu,
+          documentation = util.doc,
+        },
+
+        cmdline = {
+          enabled = true,
+          keymap = {
+            preset = "inherit",
+          },
+          completion = { menu = { auto_show = true } },
+        },
+
+        signature = {
+          enabled = false, -- Let Noice handle signature help
+        },
+
         keymap = {
-          preset = "inherit",
-          ["<Tab>"] = { "show", "accept" },
+          preset = "default",
         },
-        completion = { menu = { auto_show = true } },
-      },
-
-      signature = {
-        enabled = false, -- Let Noice handle signature help
-      },
-
-      keymap = {
-        preset = "none",
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<C-e>"] = { "hide", "fallback" },
-        ["<C-y>"] = { "select_and_accept", "fallback" },
-
-        ["<Up>"] = { "select_prev", "fallback" },
-        ["<Down>"] = { "select_next", "fallback" },
-
-        ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
-        ["<C-n>"] = { "select_next", "fallback_to_mappings" },
-
-        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
-
-        ["<Tab>"] = { "snippet_forward", "fallback" },
-        ["<S-Tab>"] = { "snippet_backward", "fallback" },
-      },
-    },
+      }
+    end,
   },
 }
