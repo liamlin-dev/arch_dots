39b895fa7192598b63c8adf2ee329b81b2b2d0e6
Liam Lin <zhonglin0107@gmail.com>
2026-04-06 13:14:52 +0800

    update

--- config/yazi/init.lua
+++
@@ -1,9 +0,0 @@
--- require("full-border"):setup()
-
--- require("yaziline"):setup({
--- 	separator_style = "curvy",
--- 	select_symbol = "",
--- 	yank_symbol = "󰆐",
--- 	filename_max_length = 24, -- trim when filename > 24
--- 	filename_trim_length = 6, -- trim 6 chars from both ends
--- })
